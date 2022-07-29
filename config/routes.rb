e78722a1bd02f8f9231f563c1258b6a11ab9a62c
samuel <sbacon803@gmail.com>
2022-07-29 11:45:16 -0600

    init and add route

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "/songs_path", controller: "all_songs", action: "index"
+end
